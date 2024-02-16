3c88ccfbf5807aea2e4190c62e32e2ca96b5140a
sukhchannu <160212424+sukhchannu@users.noreply.github.com>
2024-02-16 12:19:59 +0530

    Add files via upload

---
+++ cpp2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=5,sum;
+    sum=a+b;
+    cout<<"Sum:"<<sum;
+  
+}
---
+++ cpp5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r=10,c;
+    c=2*3.1415*r;
+    cout<<"Circumference of circle:"<<c;
+
+}
